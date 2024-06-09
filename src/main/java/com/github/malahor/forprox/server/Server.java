4a616758d36ba4c62aed12a5ae813776e178748c
Malahor <malahor1610@gmail.com>
2024-06-09 09:56:14 +0200

    removed non ssl server

--- src/main/java/com/github/malahor/forprox/server/Server.java
+++ src/main/java/com/github/malahor/forprox/server/Server.java
@@ -1,22 +1,76 @@
 package com.github.malahor.forprox.server;
 
 import java.io.IOException;
-import java.net.ServerSocket;
+import java.security.*;
+import java.security.cert.CertificateException;
+import javax.net.ssl.*;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Server {
-  private static final int PORT = 8989;
+  private static final int PORT = 9898;
+  private static final String KEY_STORE = "proxykeystore.jks";
 
-  public void start() {
-    try (var serverSocket = new ServerSocket(PORT)) {
-      log.info("Proxy server started at port: {}", PORT);
-      while (true) {
-        var clientSocket = serverSocket.accept();
-        new ProxyThread(clientSocket).start();
+  public static void main(String[] args) {
+    startProxy();
+  }
+
+  public static void startProxy() {
+    try {
+      var sslServerSocketFactory = getSslServerSocketFactory();
+      try (var serverSocket = (SSLServerSocket) sslServerSocketFactory.createServerSocket(PORT)) {
+        log.info("Proxy ssl server started at port: {}", PORT);
+        while (true) {
+          var clientSocket = serverSocket.accept();
+          new ProxyThread(clientSocket).start();
+        }
+      } catch (IOException e) {
+        log.error("Error occurred: {}", e.getMessage());
       }
-    } catch (IOException e) {
+    } catch (KeyStoreException
+        | IOException
+        | NoSuchAlgorithmException
+        | CertificateException
+        | UnrecoverableKeyException
+        | KeyManagementException e) {
       log.error("Error occurred: {}", e.getMessage());
     }
+  }
+
+  private static SSLServerSocketFactory getSslServerSocketFactory()
+      throws KeyStoreException,
+          IOException,
+          NoSuchAlgorithmException,
+          CertificateException,
+          UnrecoverableKeyException,
+          KeyManagementException {
+    var keyStore = loadKeystore();
+    var keyManagerFactory = initializeKeyManagerFactory(keyStore);
+    var sslContext = initializeSslContext(keyManagerFactory);
+    return sslContext.getServerSocketFactory();
+  }
+
+  private static KeyStore loadKeystore()
+      throws KeyStoreException, IOException, NoSuchAlgorithmException, CertificateException {
+    var keyStore = KeyStore.getInstance("JKS");
+    var classloader = Thread.currentThread().getContextClassLoader();
+    try (var keyStoreStream = classloader.getResourceAsStream(KEY_STORE)) {
+      keyStore.load(keyStoreStream, "changeit".toCharArray());
+    }
+    return keyStore;
+  }
+
+  private static KeyManagerFactory initializeKeyManagerFactory(KeyStore keyStore)
+      throws NoSuchAlgorithmException, KeyStoreException, UnrecoverableKeyException {
+    var keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+    keyManagerFactory.init(keyStore, "changeit".toCharArray());
+    return keyManagerFactory;
+  }
+
+  private static SSLContext initializeSslContext(KeyManagerFactory keyManagerFactory)
+      throws NoSuchAlgorithmException, KeyManagementException {
+    var sslContext = SSLContext.getInstance("TLS");
+    sslContext.init(keyManagerFactory.getKeyManagers(), null, null);
+    return sslContext;
   }
 }
--- src/test/java/com/github/malahor/forprox/ServerTests.java
+++ src/test/java/com/github/malahor/forprox/ServerTests.java
@@ -3,36 +3,59 @@ package com.github.malahor.forprox;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.github.malahor.forprox.server.Server;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import org.apache.hc.client5.http.ClientProtocolException;
 import org.apache.hc.client5.http.classic.HttpClient;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.BasicHttpClientConnectionManager;
 import org.apache.hc.client5.http.impl.routing.DefaultProxyRoutePlanner;
-import org.apache.hc.core5.http.HttpHost;
-import org.apache.hc.core5.http.HttpStatus;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.apache.hc.client5.http.socket.ConnectionSocketFactory;
+import org.apache.hc.client5.http.socket.PlainConnectionSocketFactory;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.core5.http.*;
+import org.apache.hc.core5.http.config.RegistryBuilder;
+import org.apache.hc.core5.ssl.SSLContexts;
+import org.apache.hc.core5.ssl.TrustStrategy;
+import org.junit.jupiter.api.*;
 
 class ServerTests {
 
-  private static final int proxyPort = 8989;
-  private static final Server server = new Server();
+  private static final int proxyPort = 9898;
   private static Thread proxyThread;
   private static HttpClient httpClient;
 
   @BeforeAll
   public static void setUp() throws Exception {
-    proxyThread = new Thread(server::start);
+    proxyThread = new Thread(Server::startProxy);
     proxyThread.start();
     // Wait for proxy to start up
     Thread.sleep(5000);
     initializeHttpClient();
   }
 
-  public static void initializeHttpClient() {
-    var proxyRoutePlanner = new DefaultProxyRoutePlanner(new HttpHost("localhost", proxyPort));
-    httpClient = HttpClients.custom().setRoutePlanner(proxyRoutePlanner).build();
+  public static void initializeHttpClient()
+      throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+    TrustStrategy acceptingTrustStrategy = (cert, authType) -> true;
+    var sslContext = SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+    var sslSocketFactory =
+        new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
+    var socketFactoryRegistry =
+        RegistryBuilder.<ConnectionSocketFactory>create()
+            .register("https", sslSocketFactory)
+            .register("http", new PlainConnectionSocketFactory())
+            .build();
+    var connectionManager = new BasicHttpClientConnectionManager(socketFactoryRegistry);
+    var proxy = new HttpHost("https", "localhost", proxyPort);
+    var routePlanner = new DefaultProxyRoutePlanner(proxy);
+    httpClient =
+        HttpClients.custom()
+            .setConnectionManager(connectionManager)
+            .setRoutePlanner(routePlanner)
+            .build();
   }
 
   @AfterAll
